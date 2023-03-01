0ad4ea2b7a11828dbb8a708ec2fea176ae31d836
DESKTOP-KSB7AKQ\jeff1212 <zeratool0207@naver.com>
2023-03-01 17:02:19 +0900

    0301 1702 proceeding...

--- src/App.js
+++ src/App.js
@@ -5,9 +5,12 @@ import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [ isLoading, setIsLoading ] = useState(false);
+
 
   async function fetchMovieHandler() {
-    const reponse = await fetch('https://swapi.dev/api/films/');
+    setIsLoading(true);
+    const response = await fetch('https://swapi.dev/api/films/');
     const data = await response.json();
 
     const transformedMovies = data.results.map(movieData => {
@@ -19,6 +22,7 @@ function App() {
       }
     })
     setMovies(transformedMovies);
+    setIsLoading(false);
   }
 
   return (
@@ -27,7 +31,9 @@ function App() {
         <button onClick={fetchMovieHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={ movies } />
+        { !isLoading && movies.length > 0 && <MoviesList movies={ movies } />}
+        { !isLoading && movies.length === 0 && <p>Found no movies.</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
